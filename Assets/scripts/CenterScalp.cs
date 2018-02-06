2c14a3bf6e65fdd6cce118995b25a527a0f5dd77
TBartl <tbartl@umich.edu>
2018-02-06 12:28:48 -0500

    Rewrote scalp positioning script

---
+++ Assets/scripts/CenterScalp.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+// Makes the center of this gameobject the center of all of the points.
+// Must be run for scalp placement to work
+public class CenterScalp : MonoBehaviour {
+
+	public List<Transform> points;
+
+	[ContextMenu("Center")]
+	public void Center() {
+		Vector3 center = Vector3.zero;
+		foreach (Transform point in points) {
+			center += point.transform.position;
+		}
+		center /= points.Count;
+
+		Transform[] children = this.transform.GetComponentsInChildren<Transform>();
+		foreach (Transform child in children) {
+			child.parent = null;
+		}
+
+		this.transform.position = center;
+
+		foreach (Transform child in children) {
+			child.parent = this.transform;
+		}
+	}
+}
