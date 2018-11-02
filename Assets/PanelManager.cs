e51e5a78816571c023cda5ec31124c0608fd51c0
kortemey <kortemey@umich.edu>
2018-11-02 16:14:52 -0400

    Implemented PanelManager

--- Assets/PanelManager.cs
+++ Assets/PanelManager.cs
@@ -3,13 +3,27 @@ using System.Collections;
 
 public class PanelManager : MonoBehaviour {
 
+    public RectTransform activePanel;
+    public RectTransform goalPoint;
+
 	// Use this for initialization
 	void Start () {
-	
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    public void activatePanel(RectTransform cur_panel) {
+        if (activePanel)
+        {
+            activePanel.localPosition = Vector2.up * 10000;
+        }
+        activePanel = cur_panel;
+        activePanel.localPosition = goalPoint.localPosition;
+
+
+    }
 }
