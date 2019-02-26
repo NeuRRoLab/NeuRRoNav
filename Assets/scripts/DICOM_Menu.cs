47ef87de0371b5d42dbcdae05411caa35233befd
kortemey <37305951+kortemey@users.noreply.github.com>
2019-02-26 15:02:25 -0500

    Created DICOM Menu placeholder stub

---
+++ Assets/scripts/DICOM_Menu.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DICOM_Menu : MonoBehaviour {
+	bool hidden;
+	Vector3 defaultPos;
+	GameObject menu;
+	CameraController camController;
+	// Use this for initialization
+	void Start () {
+		menu = GameObject.Find("DICOM_Menu");
+		camController = GameObject.Find("Camera Controller").GetComponent<CameraController>();
+
+		hidden = true;
+		defaultPos = menu.transform.localPosition;
+
+		Hide();
+		Hide();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Hide()
+	{
+		hidden = !hidden;
+		if (hidden)
+		{
+			menu.transform.localPosition = defaultPos;
+			camController.SetListenToMouse(true);
+		}
+		else
+		{
+			menu.transform.localPosition = Vector3.zero;
+			camController.SetListenToMouse(false);
+		}
+	}
+}
