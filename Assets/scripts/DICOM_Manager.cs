6eecd5b1e8f27cadf880c38d73e92cec193de0ac
kortemey <37305951+kortemey@users.noreply.github.com>
2019-02-26 16:33:50 -0500

    FO-DICOM import

--- Assets/scripts/DICOM_Manager.cs
+++ Assets/scripts/DICOM_Manager.cs
@@ -25,11 +25,32 @@ public class DICOM_Manager : MonoBehaviour {
 	}
 
 	public void LoadDICOMFromFolder(){
-		string filename = folderloc.text;
-		if (!Directory.Exists (filename)) {
+		/*
+		string dirname = folderloc.text;
+		if (!Directory.Exists (dirname)) {
 			Debug.LogError ("Could not find directory at specified location");
 			return;
 		}
+
+		if (dirname[dirname.Length - 1] != '/') {
+			dirname = dirname + "/";
+		}
+
+		DirectoryInfo d = new DirectoryInfo(dirname);
+		FileInfo[] Files = d.GetFiles();
+
+		Debug.Log(dirname+Files[0].Name);
+		image = new DicomImage(dirname+Files[0].Name);*/
+
+		//var PatientName= image.Dataset.Get<string>(Dicom.DicomTag.PatientName);
+
+		//Debug.Log(PatientName);
+		      
+		string thispath = "C:/Users/Daniel/Downloads/MR_Head_DICOM/ScalarVolume_10/";
+		DirectoryInfo d = new DirectoryInfo(thispath);//Assuming Test is your Folder
+		FileInfo[] Files = d.GetFiles(); //Getting Text files
+
+		image = new DicomImage(thispath + Files[0].Name);
 
 	}
 }
