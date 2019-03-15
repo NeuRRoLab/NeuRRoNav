25534c31b0f26cd11f9f1c3838d26e4a0744f4d9
kortemey <37305951+kortemey@users.noreply.github.com>
2019-03-15 14:16:01 -0400

    Refactor into DICOMImgSpecs class

--- Assets/scripts/DICOM_Manager.cs
+++ Assets/scripts/DICOM_Manager.cs
@@ -9,18 +9,18 @@ using System.Collections.Generic;
 
 public class DICOM_Manager : MonoBehaviour {
 	// Handles the loading and representation of DICOM files.
+
+	// Note: when I say z axis, I mean from back to front of head
+	// 		 when I say x axis, I mean from left to right of head
+	// 		 when I say y axis, I mean from bottom to top of head.
+
 	public InputField folderloc;
 
-	DicomImage image;
-	IPixelData pixelData;
+	DICOMImgSpecs imgspecs;
 
 	// Use this for initialization
 	void Start () {
-		/*string thispath = "C:/Users/Daniel/Downloads/MR_Head_DICOM/ScalarVolume_10/";
-		DirectoryInfo d = new DirectoryInfo(thispath);//Assuming Test is your Folder
-		FileInfo[] Files = d.GetFiles(); //Getting Text files
-
-		image = new DicomImage(thispath + Files[0].Name);*/
+		
 	}
 	
 	// Update is called once per frame
@@ -29,7 +29,44 @@ public class DICOM_Manager : MonoBehaviour {
 	}
 
 	public void LoadDICOMFromFolder(){
-		string dirname = folderloc.text;
+		imgspecs = new DICOMImgSpecs ();
+		imgspecs.InitFromDir(folderloc.text);
+	}
+}
+
+struct Vector3Int{
+	public int x;
+	public int y;
+	public int z;
+
+	public Vector3Int(int _x,int _y, int _z){
+		x = _x;
+		y = _y;
+		z = _z;
+	}
+}
+
+class DICOMImgSpecs{
+	public Vector3 originpos;
+
+	public Vector3 row_dir_cosine;
+	public Vector3 col_dir_cosine;
+	public Vector3 slice_dir_cosine;
+
+	public Vector3 dicomspace_dims; // Length width height in millimeters, essentially the 
+									// rectangular prism the thing occupies.
+
+	public float[] voxelarr;
+
+	public float maxval;
+
+	public int rows;
+	public int cols;
+	public int slices;
+
+	FileInfo[] dicom_files;
+
+	public void InitFromDir(string dirname){
 		if (dirname[dirname.Length - 1] != '/') {
 			dirname = dirname + "/";
 		}
@@ -38,20 +75,95 @@ public class DICOM_Manager : MonoBehaviour {
 			Debug.LogError ("Could not find directory at specified location");
 			return;
 		}
-
+			
+		// Need to get initial stuff. 
 		DirectoryInfo d = new DirectoryInfo(dirname);
-		FileInfo[] Files = d.GetFiles();
+		dicom_files = d.GetFiles();
 
-		Debug.Log(dirname+Files[0].Name);
-		image = new DicomImage(dirname+Files[0].Name);
+		var image = new DicomImage(dirname+dicom_files[0].Name);
+		var nextimage = new DicomImage(dirname+dicom_files[1].Name);
 
-		string PatientName= image.Dataset.Get<string>(Dicom.DicomTag.PatientName);
 		string[] imagepospatient = image.Dataset.Get<string[]>(Dicom.DicomTag.ImagePositionPatient);
 		string[] imageorientationpatient = image.Dataset.Get<string[]> (Dicom.DicomTag.ImageOrientationPatient);
+		string[] nextpospatient = nextimage.Dataset.Get<string[]>(Dicom.DicomTag.ImagePositionPatient);
 
+		// Assign Basic Image Vars
+		originpos = new Vector3 (
+			System.Convert.ToSingle(imagepospatient[0]),
+			System.Convert.ToSingle(imagepospatient[1]),
+			System.Convert.ToSingle(imagepospatient[2]));
 
-		foreach (var cur in imageorientationpatient) {
-			Debug.Log (cur);
+		row_dir_cosine = new Vector3 (
+			System.Convert.ToSingle(imageorientationpatient[0]),
+			System.Convert.ToSingle(imageorientationpatient[1]),
+			System.Convert.ToSingle(imageorientationpatient[2]));
+
+		col_dir_cosine = new Vector3 (
+			System.Convert.ToSingle(imageorientationpatient[3]),
+			System.Convert.ToSingle(imageorientationpatient[4]),
+			System.Convert.ToSingle(imageorientationpatient[5]));
+
+		Vector3 newpos = new Vector3 (
+			System.Convert.ToSingle(nextpospatient[0]),
+			System.Convert.ToSingle(nextpospatient[1]),
+			System.Convert.ToSingle(nextpospatient[2]));
+
+		slice_dir_cosine = newpos - originpos;
+
+		rows = image.Dataset.Get<int>(Dicom.DicomTag.Rows);
+		cols = image.Dataset.Get<int>(Dicom.DicomTag.Columns);
+		slices = dicom_files.Length;
+
+		dicomspace_dims = (
+			row_dir_cosine * rows+
+			col_dir_cosine * cols+
+			slice_dir_cosine * slices);	
+
+		LoadInPixelData ();
+
+		printSpecs ();
+	
+	}
+
+	void LoadInPixelData(){
+		voxelarr = new float[rows*cols*slices];
+
+		IPixelData pixelData;
+
+		int fileindex = 0;
+		foreach (FileInfo file in dicom_files)
+		{
+			Texture2D newtex = new Texture2D(rows, cols);
+
+			var image = new DicomImage(file.FullName);
+			pixelData = PixelDataFactory.Create(image.PixelData, 0);
+
+			for (int y = 0; y < cols; ++y)
+			{
+				for (int x = 0; x < rows; ++x)
+				{
+					float cur_pixel = (float)pixelData.GetPixel(x, y);
+					voxelarr [(rows * cols) * fileindex + (y * rows) + x] = cur_pixel;
+					if (cur_pixel > maxval) {
+						maxval = cur_pixel;
+					}
+
+				}
+			}
+			fileindex += 1;
+			//Debug.Log ("Maxval: "+maxval.ToString ());
 		}
+
+	}
+
+	public void printSpecs(){
+		Debug.Log ("Origin:" + originpos.ToString ());
+		Debug.Log ("Row:" + row_dir_cosine.ToString ());
+		Debug.Log ("Col:" + col_dir_cosine.ToString ());
+		Debug.Log ("Slice:" + slice_dir_cosine.ToString ());
+		Debug.Log ("Rows:" + rows.ToString ());
+		Debug.Log ("Cols:" + cols.ToString ());
+		Debug.Log ("Slices:" + slices.ToString ());
+		Debug.Log ("DicomSpaceDims:" + dicomspace_dims.ToString ());
 	}
 }
